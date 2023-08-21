6f922493b8c2e1e05ce0e5f65b7233d709c7bb4f
Artemii Akhunov <artemii.akhunov@gmail.com>
2023-08-21 16:15:09 -0400

    added support for wpa2 enterprise wifi networks

--- install.sh
+++ install.sh
@@ -4,11 +4,42 @@ sudo pacman -Syu
 
 # Installing neessary packages
 
-sudo pacman -S xorg xorg-xinit pulseaudio sof-firmware alsa i3-gaps alacritty rofi firefox lightdm lightdm-gtk-greeter feh rustup gnome-themes-extra
+sudo pacman -S xorg xorg-xinit pulseaudio sof-firmware alsa i3-gaps alacritty rofi firefox lightdm lightdm-gtk-greeter feh rustup gnome-themes-extra nm-connection-editor modemmanager usb_modeswitch ca-certificates
 
 # Enabling lightdm
 
 systemctl enable lightdm
+
+# Enabling Modem Manager for phone modem wifi
+
+systemctl enable ModemManager
+systemctl start ModemManager
+systemctl restart NetworkManager
+
+# Creating ca-certificates
+
+sudo update-ca-trust force-enable
+
+sudo cp /etc/ca-certificates/extracted/*.pem /etc/ca-certificates/trust-source/anchors
+
+sudo update-ca-trust
+
+# Adding WPA suplicant to NetworkManager
+
+sudo cp /etc/ssl/openssl.cnf /etc/wpa_supplicant/.
+
+# Editing WPA files, so that wpa2 enterprise will work
+
+sudo sed -i '/providers = provider_sect/a ssl_conf = ssl_sect\n\n[ssl_sect]\nsystem_default = system_default_sect\n\n[system_default_sect]\nOptions = UnsafeLegacyRenegotiation\nCipherString = DEFAULT@SECLEVEL1' /etc/wpa_supplicant/openssl.cnf
+
+# Editing WPA suplicant service
+
+sudo sed -i '/BusName=fi.w1.wpa_supplicant1/a Environment="OPENSSL_CONF=/etc/wpa_supplicant/openssl.conf"' /usr/lib/systemd/system/wpa_supplicant.service
+
+# NetworkManager
+
+sudo sed -i '/for details./a [device]\nwifi.scan-rand-mac-address=no' /etc/NetworkManager/NetworkManager.conf
+
 
 # Making necessery folders
 
