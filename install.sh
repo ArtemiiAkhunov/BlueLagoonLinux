c15c9c0584e95d58d6c0c5ef31b5b72659624cfa
Artemii Akhunov <artemii.akhunov@gmail.com>
2023-08-23 18:07:21 -0400

    added a command line spotify client

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo pacman -Syu
 
 # Installing neessary packages
 
-sudo pacman -S xorg xorg-xinit pulseaudio pulseaudio-alsa pulseaudio-jack sof-firmware alsa pavucontrol pamixer brightnessctl i3-gaps kitty rofi firefox lightdm lightdm-gtk-greeter feh rustup gnome-themes-extra nm-connection-editor modemmanager usb_modeswitch ca-certificates ttf-font-awesome ttf-firacode-nerd
+sudo pacman -S xorg xorg-xinit pulseaudio pulseaudio-alsa pulseaudio-jack sof-firmware alsa pavucontrol pamixer ncspot brightnessctl i3-gaps kitty rofi firefox lightdm lightdm-gtk-greeter feh rustup gnome-themes-extra nm-connection-editor modemmanager usb_modeswitch ca-certificates ttf-font-awesome ttf-firacode-nerd jq
 
 # Enabling and configuring lightdm
 
